9e346b0dd0ef38a59666950a995d29407de0986b
Sebastian Kotstein <sebastian.kotstein@gmx.de>
2024-01-29 16:18:24 +0100

    Alphabetical sorting of schema value

--- tools/pipeline/pipeline.py
+++ tools/pipeline/pipeline.py
@@ -38,6 +38,7 @@ class Pipeline:
             self.cache = None
     
     def process(self, input_dict, top = None, suppress_duplicates = False):
+        input_dict = self.sort_schema_values(input_dict)
         batch = self.json_to_batch(input_dict)
         if len(batch["qa_sample_id"]):
             tokenized_samples = self.tokenizer.tokenize(batch)
@@ -47,6 +48,16 @@ class Pipeline:
             results = self.interpreter.create_empty_results_dict()
         merged_output = self.merge_results_w_input_json(input_dict,results)
         return self.limit_results(merged_output,top,suppress_duplicates)
+    
+    def sort_schema_values(self, input_dict):
+        if "schemas" in input_dict:
+            for i,schema in enumerate(input_dict["schemas"]):
+                if "value" in schema:
+                    value = input_dict["schemas"][i]["value"].strip().split()
+                    value.sort()
+                    input_dict["schemas"][i]["value"] = " ".join(value)
+        return input_dict
+
 
     def limit_results(self, input_dict, top = None, suppress_duplicates = False):
         for schema in input_dict["schemas"]:
@@ -77,57 +88,57 @@ class Pipeline:
     
     def json_to_batch(self, input_dict):
         
-        if self.cache:
-            batch = {
-                "qa_sample_id":[],
-                "qa_sample_title":[],
-                "qa_sample_query":[],
-                "qa_sample_paragraph_id":[],
-                "qa_sample_paragraph_title":[],
-                "qa_sample_paragraph":[],
-                "verbose_output":[]
-            }
-            if "schemas" not in input_dict:
-                raise InvalidRequestException("The request does not contain a list of schemas, i.e., '$.schemas[*]'")
-            if not len(input_dict["schemas"]):
-                raise InvalidRequestException("The list of schemas, i.e., '$.schemas[*]', must contain at least one schema item")
+        batch = {
+            "qa_sample_id":[],
+            "qa_sample_title":[],
+            "qa_sample_query":[],
+            "qa_sample_paragraph_id":[],
+            "qa_sample_paragraph_title":[],
+            "qa_sample_paragraph":[],
+            "verbose_output":[]
+        }
+        if "schemas" not in input_dict:
+            raise InvalidRequestException("The request does not contain a list of schemas, i.e., '$.schemas[*]'")
+        if not len(input_dict["schemas"]):
+            raise InvalidRequestException("The list of schemas, i.e., '$.schemas[*]', must contain at least one schema item")
 
-            for i,schema in enumerate(input_dict["schemas"]):
-                if "value" not in schema:
-                    raise InvalidRequestException("The schema '$.schemas["+str(i)+"]' has no property 'value'")
-                if not schema["value"]:
-                    raise InvalidRequestException("'$.schemas["+str(i)+"].value' must not be empty")
-                if "schemaId" not in schema:
-                    schema["schemaId"] = str(uuid.uuid4())
-                if "name" not in schema:
-                    schema["name"] = "schema "+schema["schemaId"]
+        for i,schema in enumerate(input_dict["schemas"]):
+            if "value" not in schema:
+                raise InvalidRequestException("The schema '$.schemas["+str(i)+"]' has no property 'value'")
+            if not schema["value"]:
+                raise InvalidRequestException("'$.schemas["+str(i)+"].value' must not be empty")
+            if "schemaId" not in schema:
+                schema["schemaId"] = str(uuid.uuid4())
+            if "name" not in schema:
+                schema["name"] = "schema "+schema["schemaId"]
 
-                if "queries" not in schema:
-                    raise InvalidRequestException("The schema '$.schemas["+str(i)+"]' has no list of queries, i.e., '$.schemas["+str(i)+"].queries[*]'")
-                if not len(schema["queries"]):
-                    raise InvalidRequestException("'$.schemas["+str(i)+"].queries[*]' must contain at least one query item")
-                
-                for j,query in enumerate(schema["queries"]):
-                    if "value" not in query:
-                        raise InvalidRequestException("The query '$.schemas["+str(i)+"].queries["+str(j)+"]' has no property 'value'")
-                    if not query["value"]:
-                        raise InvalidRequestException("'$.schemas["+str(i)+"].queries["+str(j)+"].value' must not be empty")
-                    if "queryId" not in query:
-                        query["queryId"] = str(uuid.uuid4())
-                    if "name" not in query:
-                        query["name"] = "query "+query["queryId"]
-                    if "verboseOutput" not in query:
-                        query["verboseOutput"] = False
+            if "queries" not in schema:
+                raise InvalidRequestException("The schema '$.schemas["+str(i)+"]' has no list of queries, i.e., '$.schemas["+str(i)+"].queries[*]'")
+            if not len(schema["queries"]):
+                raise InvalidRequestException("'$.schemas["+str(i)+"].queries[*]' must contain at least one query item")
+            
+            for j,query in enumerate(schema["queries"]):
+                if "value" not in query:
+                    raise InvalidRequestException("The query '$.schemas["+str(i)+"].queries["+str(j)+"]' has no property 'value'")
+                if not query["value"]:
+                    raise InvalidRequestException("'$.schemas["+str(i)+"].queries["+str(j)+"].value' must not be empty")
+                if "queryId" not in query:
+                    query["queryId"] = str(uuid.uuid4())
+                if "name" not in query:
+                    query["name"] = "query "+query["queryId"]
+                if "verboseOutput" not in query:
+                    query["verboseOutput"] = False
 
-                    if not self.cache.has(schema["value"],query["value"],query["verboseOutput"]):
-                        batch["qa_sample_id"].append(query["queryId"])
-                        batch["qa_sample_title"].append(query["name"])
-                        batch["qa_sample_query"].append(query["value"])
-                        batch["qa_sample_paragraph_id"].append(schema["schemaId"])
-                        batch["qa_sample_paragraph_title"].append(schema["name"])
-                        batch["qa_sample_paragraph"].append(schema["value"])
-                        batch["verbose_output"].append(query["verboseOutput"])
-            return batch
+                if not self.cache or (self.cache and not self.cache.has(schema["value"],query["value"],query["verboseOutput"])):
+                    batch["qa_sample_id"].append(query["queryId"])
+                    batch["qa_sample_title"].append(query["name"])
+                    batch["qa_sample_query"].append(query["value"])
+                    batch["qa_sample_paragraph_id"].append(schema["schemaId"])
+                    batch["qa_sample_paragraph_title"].append(schema["name"])
+                    batch["qa_sample_paragraph"].append(schema["value"])
+                    batch["verbose_output"].append(query["verboseOutput"])
+        return batch
+        '''
         else:
             return {
                 "qa_sample_id":[query["queryId"] for schema in input_dict["schemas"] for query in schema["queries"]],
@@ -138,6 +149,7 @@ class Pipeline:
                 "qa_sample_paragraph": [schema["value"] for schema in input_dict["schemas"] for _ in schema["queries"]],
                 "verbose_output": [query["verboseOutput"] for schema in input_dict["schemas"] for query in schema["queries"]]
             }
+        '''
     
     def merge_results_w_input_json(self, input_dict, results):
         for schema in input_dict["schemas"]:
